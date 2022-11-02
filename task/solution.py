b890ec31c4c598b4b563911c121381502e3a4279
itambu <ivan.ignatenko@gmail.com>
2022-11-02 19:55:24 +0300

    changes

--- task/solution.py
+++ task/solution.py
@@ -1,6 +1,12 @@
 def get_gold_count(mines: list[list[int]]) -> int:
     """
-    :param mines: a list of lists of integers. Each entry represents itself the number of gold
+    This function must compute the number of gold that can be gathered by a miner.
+    Each entry in this input matrix `mines` contains a positive integer which is the amount of gold.
+    The miner starts at any cell of the first column and must finish his path in the last column.
+    The miner can move only right or right down from a cell to the next one. The aim is to find out
+    the maximum amount of gold that can be gathered.
+
+    :param mines: a list of lists of integers. Each entry represents the number of gold in a cell
     :return: the maximum number of gold which a miner can gather by taking into account the rules
     """
     pass
