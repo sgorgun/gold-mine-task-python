cbcf4636e6ecab46c957bde5054693e51a4433c7
Sergei Gorgun <sergei.gorgun@gmail.com>
2024-05-27 15:03:14 +0300

    Done

--- task/gold_mine.py
+++ task/gold_mine.py
@@ -14,4 +14,19 @@ def get_maximum_amount_of_gold(mines: List[List[int]]) -> int:
     Returns:
         int, the maximum amount of gold that can be gathered.
     """
-    pass
+    num_rows = len(mines)
+    num_cols = len(mines[0])
+    goldMatrix = [[0] * num_cols for _ in range(num_rows)]
+    
+    for row in range(num_rows):
+        goldMatrix[row][0] = mines[row][0]
+    
+    for col in range(1, num_cols):
+        for row in range(num_rows):
+            if row == 0:
+                goldMatrix[row][col] = mines[row][col] + goldMatrix[row][col-1]
+            else:
+                goldMatrix[row][col] = mines[row][col] + max(goldMatrix[row][col-1], goldMatrix[row-1][col-1])
+    
+    max_gold = max(goldMatrix[row][num_cols-1] for row in range(num_rows))
+    return max_gold
